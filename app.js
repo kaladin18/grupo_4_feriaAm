1d118282832dba9747c996c07a82ac4ef00fd20e
kaladin18 <francosantamaria.sm@gmail.com>
2022-02-05 16:32:48 -0300

    segundo commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"./public")));
+
+app.listen(3000, () => console.log("servidor corriendo en puerto 3000"));
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/index.html")));
