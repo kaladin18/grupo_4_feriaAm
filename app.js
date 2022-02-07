7c1f21ad138e819deefc94eafad6358e83ff4ede
Marco <marcosparv@live.com.ar>
2022-02-07 15:09:57 -0300

    se crearon los html de register y login falta dar estilos

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.use(express.static(path.resolve(__dirname,"./public")));
 app.listen(3000, () => console.log("servidor corriendo en puerto 3000"));
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/index.html")));
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
