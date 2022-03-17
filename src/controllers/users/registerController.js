f19a7cb896cbd36f65113c7a73b74cb0c67030f4
kaladin18 <francosantamaria.sm@gmail.com>
2022-03-17 18:19:42 -0300

    agregado post de registro exitoso

--- src/controllers/users/registerController.js
+++ src/controllers/users/registerController.js
@@ -1,5 +1,10 @@
 module.exports = {
     main: function(req, res) {
         res.render("users/register");
-    }
+    },
+    success: function (req, res) {
+        res.render("users/registroexitoso");
+    } 
+        
+    
 }
--- src/routes/users/register.routes.js
+++ src/routes/users/register.routes.js
@@ -3,5 +3,5 @@ const registerController = require("../../controllers/users/registerController")
 const router = express.Router();
 
 router.get("/", registerController.main);
-
+router.post("/success", registerController.success);
 module.exports = router;
