00bf8812efdc6cf9b72c03f3144dbc1f96a034a9
kaladin18 <francosantamaria.sm@gmail.com>
2022-04-07 02:14:15 -0300

    productos tienen sellerID y muestran opcion de editar o eliminar en vez de comprar dependiendo del usuario

---
+++ src/middlewares/productSellerAuthMiddleware.js
@@ -0,0 +1,18 @@
+const Product = require("../models/Product");
+
+
+
+function productSellerAuthMiddleware (req, res, next) {
+    let loggedUser = req.session.loggedUser;
+    let product = Product.findByPK(req.params.id);
+
+    
+    if (loggedUser.id != product.sellerID) {
+        return res.redirect("/");
+    }
+   
+
+    next();
+}
+
+module.exports = productSellerAuthMiddleware;
--- src/routes/products/products.routes.js
+++ src/routes/products/products.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const productsController = require("../../controllers/products/productsController");
 const authMiddleware = require("../../middlewares/authMiddleware");
+const productSellerAuthMiddleware = require("../../middlewares/productSellerAuthMiddleware");
+
 const router = express.Router();
 
 
@@ -10,7 +12,7 @@ router.get("/cart", productsController.cart);
 router.get("/db/:id", productsController.detail);
 //creacion y edicion de productos
 router.get("/create", authMiddleware, productsController.create);
-router.get("/db/:id/edit", authMiddleware, productsController.editView);
+router.get("/db/:id/edit", authMiddleware, productSellerAuthMiddleware, productsController.editView);
 router.put("/db/:id", productsController.editPUT);
 router.delete("/db/:id", productsController.deleteProduct);
 
